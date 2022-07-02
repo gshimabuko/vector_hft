1d597aee03bbd88a8467fe9ea12b8c5647d118cf
gshimabuko <gshimabuko@gmail.com>
2022-07-02 18:57:32 -0300

    mend

--- testbench.v
+++ testbench.v
@@ -1,14 +1,18 @@
 // Testbench top
-module tb;
-logic clock;
-logic reset;
-logic trigger_write_enable;
-logic trigger_side;
-logic trigger_direction;
-logic [7:0] trigger_price;
-logic [7:0] bid_price;
-logic [7:0] ask_price;
+`timescale 1 ns / 1ps
+module tb();
+parameter CLK_HALF_PERIOD = 10;
+reg clock;
+reg reset;
+reg trigger_write_enable;
+reg trigger_side;
+reg trigger_direction;
+reg [7:0] trigger_price;
+reg [7:0] bid_price;
+reg [7:0] ask_price;
+
 wire trigger_satisfied;
+
 trigger trigger_inst (
 .clock(clock)
 , .reset(reset)
@@ -20,9 +24,13 @@ trigger trigger_inst (
 , .ask_price(ask_price)
 , .trigger_satisfied(trigger_satisfied)
 );
+
 initial begin
 clock = 0;
 reset = 0;
-//...
+
+    #0 trigger_write_enable = 0; trigger_side = 0;
+    #2*CLK_HALF_PERIOD
 end
+always #CLK_HALF_PERIOD clock = ~clock;
 endmodule
